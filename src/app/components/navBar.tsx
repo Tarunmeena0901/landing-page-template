de998c530e6c9b2fa2ec1e58c9db15217f217c54
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-06-25 00:43:57 +0530

    navBar update

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react"
 export function NavBar() {
     return (
         <div className=" absolute sticky top-5 h-[4vh] flex items-center justify-center bg-transparent w-full">
-            <div className=" relative text-white flex justify-center items-center rounded w-[70vh] h-[5vh] backdrop-blur bg-slate-600/30 origin-center mx-auto ">
+            <div className=" relative text-white flex justify-center items-center rounded w-[70vh] h-[5vh] backdrop-blur bg-slate-600/30 origin-center mx-auto border-2 border-slate-200 ">
                 <Tabs>Product</Tabs>
                 <Tabs>Price</Tabs>
                 <Tabs>Company</Tabs>
