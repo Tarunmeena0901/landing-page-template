fc977d1b7c910e2072685d7a02006ef9d13a80bc
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-06-30 03:23:20 +0530

    scroll light reveal effect added in client component

--- src/app/components/sections/ClientSection.tsx
+++ src/app/components/sections/ClientSection.tsx
@@ -1,8 +1,41 @@
+"use client"
+import { motion, useInView, useMotionTemplate, useScroll, useTransform } from "framer-motion"
+import { useRef } from "react"
+
 export function ClientSection () {
+    const ref = useRef(null);
+    // const {scrollYProgress} = useScroll({
+    //     target: ref,
+    //     offset: ['start start','end end']
+    // })
+
+    // const lightRevealProgress = useTransform(scrollYProgress, [0,100], ['0%','100%']);
+    // const background = useMotionTemplate`linear-gradient(to right, black ${40 - lightRevealProgress}%, transparent 50%, black 60%)`
+
+    const isInView = useInView(ref, { amount: 0.75 ,once: true })
+
     return (
-        <div className="relative bg-neutral-900 h-screen ">
-            <div className="absolute bg-gradient-to-r from-black/30 from-20% via-transparent via-50% to-black to-90% inset-0"></div>
-            hello
-        </div>
+        <section 
+        ref={ref}
+        className="relative bg-neutral-900 h-screen ">
+            <motion.div
+            animate={{
+                background: isInView ? 'linear-gradient(90deg, rgba(0,0,0,1) 5%, rgba(0,0,0,0) 50%, rgba(0,0,0,1) 95%) no-repeat' :
+                'linear-gradient(90deg, rgba(0,0,0,1) 40%, rgba(0,0,0,0) 50%, rgba(0,0,0,1) 60%) no-repeat'
+            }}
+            transition={{
+                duration: 2
+            }}
+            className="absolute inset-0">
+            </motion.div>
+            <motion.div
+            animate={{
+                filter: isInView ? "blur(0px)" : "blur(10px)"
+            }}
+            className="h-full grid place-content-center text-3xl font-bold"
+            >
+                HELLLOO
+            </motion.div>
+        </section>
     )
 }
